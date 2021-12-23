450170cb75f88c4c9edb3fad9f499f95c00b3049
Behar Kadri <kadri.behar@gmail.com>
2021-12-23 03:17:52 +0100

    User location, map bounds, distance from user to institution initial implementation

--- src/components/MapSection/MapSection.js
+++ src/components/MapSection/MapSection.js
@@ -5,6 +5,21 @@ import LeaveReview from '../LeaveReview/LeaveReview';
 import AuthContext from '../../context/auth-context';
 import { useHistory } from 'react-router-dom';
 import SeeReviews from '../SeeReviews/SeeReviews';
+import useGeoLocation from '../../hooks/use-geolocation';
+import L from 'leaflet';
+
+import userMarker from '../../assets/Icons/marker.png';
+
+const markerIcon = new L.Icon({
+  iconUrl: userMarker,
+  iconSize: [40, 40],
+  iconAnchor: [17, 46], //[left/right, top/bottom]
+  popupAnchor: [0, -46], //[left/right, top/bottom]
+});
+
+const corner1 = L.latLng(40.5776464, 20.0139859);
+const corner2 = L.latLng(42.5040261, 23.3349122);
+const bounds = L.latLngBounds(corner1, corner2);
 
 const MapSection = ({ institutions }) => {
   const [modal, setModal] = useState(false);
@@ -14,8 +29,9 @@ const MapSection = ({ institutions }) => {
   const [institutionId, setInstitutionId] = useState('');
   const [institutionId1, setInstitutionId1] = useState('');
 
+  const location = useGeoLocation();
+
   const toggleHandler = (insId) => {
-    console.log('HANDLER CALL');
     if (authCtx.isLoggedIn) {
       setInstitutionId(insId);
       setModal(!modal);
@@ -62,12 +78,40 @@ const MapSection = ({ institutions }) => {
 
   const filteredInstitutions = filter(institutions);
 
+  function getDistance(from, to) {
+    // return distance in meters
+    var lon1 = toRadian(from.lng),
+      lat1 = toRadian(from.lat),
+      lon2 = toRadian(to.lng),
+      lat2 = toRadian(to.lat);
+
+    var deltaLat = lat2 - lat1;
+    var deltaLon = lon2 - lon1;
+
+    var a =
+      Math.pow(Math.sin(deltaLat / 2), 2) +
+      Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(deltaLon / 2), 2);
+    var c = 2 * Math.asin(Math.sqrt(a));
+    var EARTH_RADIUS = 6371;
+    var result = (c * EARTH_RADIUS * 1000).toFixed(2);
+
+    return result > 1000
+      ? (result / 1000).toFixed(2) + ' km.'
+      : result + ' meters.';
+  }
+  function toRadian(degree) {
+    return (degree * Math.PI) / 180;
+  }
+
   return (
     <>
       <MapContainer
-        center={[41.9932326, 21.4154083]}
-        zoom={10}
+        center={bounds.getCenter()}
+        zoom={9}
+        minZoom={8}
         scrollWheelZoom={true}
+        bounds={bounds}
+        maxBounds={bounds}
       >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -78,6 +122,14 @@ const MapSection = ({ institutions }) => {
             <Marker key={obj.id} position={[obj.latitude, obj.longitude]}>
               <Popup>
                 {obj.amenity} <br /> {obj.name}.<br />
+                <span>
+                  Distance:{' '}
+                  {getDistance(location.coordinates, {
+                    lat: obj.latitude,
+                    lng: obj.longitude,
+                  })}
+                </span>
+                <br />
                 <button onClick={() => toggleHandler(obj.id)}>
                   Leave a review
                 </button>
@@ -87,6 +139,14 @@ const MapSection = ({ institutions }) => {
               </Popup>
             </Marker>
           ))}
+        {location.loaded === true && (
+          <Marker
+            icon={markerIcon}
+            position={[location.coordinates.lat, location.coordinates.lng]}
+          >
+            <Popup>Your Location...</Popup>
+          </Marker>
+        )}
       </MapContainer>
       {modal === true && (
         <LeaveReview
